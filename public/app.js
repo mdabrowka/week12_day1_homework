d295251894c190e16248702f2b50a36409a78754
Marta <martadabrowka@gmail.com>
2017-12-11 17:26:15 +0000

    removed cats from html

--- public/app.js
+++ public/app.js
@@ -30,7 +30,10 @@
 
 
 var app = function() {
-  addNewCat("Name: Lola", "Favourite food: Tuna fish", `https://fthmb.tqn.com/65lNzIRNfZY4xY02D17b1RcGvso=/960x0/filters:no_upscale()/kitten-looking-at-camera-521981437-57d840213df78c583374be3b.jpg` )
+  addNewCat("Name: Boba", "Favourite food: Sock fluff", `http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg`)
+  addNewCat("Name: Barnaby", "Favourite food: Tuna", `https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg`)
+  addNewCat("Name: Max", "Favourite food: Whiskas Temptations", `http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg`)
+  addNewCat("Name: Lola", "Favourite food: Diet coke", `https://fthmb.tqn.com/65lNzIRNfZY4xY02D17b1RcGvso=/960x0/filters:no_upscale()/kitten-looking-at-camera-521981437-57d840213df78c583374be3b.jpg` )
 }
 
 
